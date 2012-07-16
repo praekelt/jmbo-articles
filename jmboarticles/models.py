2e80ce9529bad60ed1a6bc681906f421565a64bc
Milton Madanda <milton@praekeltfoundation.org>
2012-07-16 17:06:09 +0200

    Allow multiple Polls per article

--- jmboarticles/models.py
+++ jmboarticles/models.py
@@ -36,7 +36,7 @@ class Article(models.Model, DirtyFieldsMixin):
     tags = models.ManyToManyField('category.Tag', blank=True, null=True)
 
     # Extra Content
-    poll = models.ForeignKey(Poll, blank=True, null=True)
+    polls = models.ManyToManyField(Poll, blank=True, null=True, related_name='articles')
     video = models.ForeignKey(Video, blank=True, null=True)
     linked_article = models.ForeignKey('jmboarticles.Article', blank=True, null=True)
     linked_page = models.ForeignKey('flatpages.FlatPage', blank=True, null=True)
